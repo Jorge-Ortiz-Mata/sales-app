ccd77fc2a46a341629fa0716566bdb4654ba83e9
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-09-04 15:52:52 -0500

    feat: update categories without reloading the web application

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -16,15 +16,21 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.turbo_stream { render turbo_stream: turbo_stream.append('categories', partial: 'categories/category', locals: { category: @category }) }
+        format.turbo_stream { render turbo_stream: turbo_stream.append('categories', partial: 'categories/category', locals: { category: @category })}
       else
-        format.turbo_stream { render turbo_stream: turbo_stream.replace('category_form', partial: 'categories/form', locals: { category: @category, text_btn: 'Guardar' })}
+        format.turbo_stream { render turbo_stream: turbo_stream.replace('new_category_form', partial: 'categories/form', locals: { category: @category, text_btn: 'Guardar', form_id: 'new_category_form' })}
       end
     end
   end
 
   def update
-    debugger
+    respond_to do |format|
+      if @category.update category_params
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("category_#{@category.id}", partial: 'categories/category', locals: { category: @category })}
+      else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace('edit_category_form', partial: 'categories/form', locals: { category: @category, text_btn: 'Editar', form_id: 'edit_category_form' })}
+      end
+    end
   end
 
   def destroy
