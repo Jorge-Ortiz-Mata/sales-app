9f05ea0d353b3a004a3d96cbfe316764eb4c9a78
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-09-09 19:59:51 -0500

    feat: add the rails assets:precompile when the container is ready

--- production-deplyment.sh
+++ production-deplyment.sh
@@ -1,5 +1,8 @@
 # Prepare DB. If the database exists, it runs the migrations. Otherwise, it creates the database and run the migrations
 rails db:prepare
 
+# Compile assets
+rails assets:precompile
+
 # Launch Rails Server
 puma
