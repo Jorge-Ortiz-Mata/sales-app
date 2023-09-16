59ae5c84eb6012f79b307f148d9ca8e4c4f5eaa4
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-09-16 17:56:55 -0500

    design: add style to the new registrations page with the image and the form

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,12 +4,21 @@ class Users::SessionsController < Devise::SessionsController
   layout 'unauthenticated'
   # before_action :configure_sign_in_params, only: [:create]
 
-  def new
-    super
-  end
+  # def new
+  #   super
+  # end
 
   # def create
-  #   super
+  #   self.resource = warden.authenticate!(auth_options)
+  #   set_flash_message(:notice, :signed_in) if is_navigational_format?
+  #   sign_in(resource_name, resource)
+  #   if !session[:return_to].blank?
+  #     # redirect_to session[:return_to]
+  #     session[:return_to] = nil
+  #     redirect_to new_user_session_path, notice: 'kjfgkj'
+  #   else
+  #     respond_with resource, location: after_sign_in_path_for(resource)
+  #   end
   # end
 
   # DELETE /resource/sign_out
