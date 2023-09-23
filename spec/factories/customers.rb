198f52fb343b0cab0c5e4b5431039af1df1414ab
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-09-23 17:22:48 -0500

    test: update and add tests for the customer model and controller.

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -1,7 +1,9 @@
 FactoryBot.define do
   factory :customer do
-    full_name { "MyString" }
-    phone_number { "MyString" }
-    address { "MyString" }
+    trait :with_attributes do
+      full_name { 'Jorge Ortiz' }
+      phone_number { 1234567890 }
+      address { 'San Luis Potosí S.L.P. México' }
+    end
   end
 end
--- spec/policies/customer_policy_spec.rb
+++ spec/policies/customer_policy_spec.rb
@@ -1,28 +1,127 @@
 require 'rails_helper'
 
 RSpec.describe CustomerPolicy do
+  let(:viewer) { create(:user, :with_email, :with_password, :with_password_confirmation) }
+  let(:editor) { create(:user, :with_email, :with_password, :with_password_confirmation, role: 1) }
+  let(:admin) { create(:user, :with_email, :with_password, :with_password_confirmation, role: 2) }
 
-  let(:user) { User.new }
+  describe 'as an admin user' do
+    subject { described_class }
 
-  subject { described_class }
+    permissions :index? do
+      it 'should allow this action' do
+        expect(subject).to permit(admin)
+      end
+    end
 
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
+    permissions :new? do
+      it 'should allow this action' do
+        expect(subject).to permit(admin)
+      end
+    end
+
+    permissions :edit? do
+      it 'should allow this action' do
+        expect(subject).to permit(admin)
+      end
+    end
+
+    permissions :create? do
+      it 'should allow this action' do
+        expect(subject).to permit(admin)
+      end
+    end
+
+    permissions :update? do
+      it 'should allow this action' do
+        expect(subject).to permit(admin)
+      end
+    end
+
+    permissions :destroy? do
+      it 'should allow this action' do
+        expect(subject).to permit(admin)
+      end
+    end
   end
 
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  describe 'as an editor user' do
+    subject { described_class }
+
+    permissions :index? do
+      it 'should allow this action' do
+        expect(subject).to permit(editor)
+      end
+    end
+
+    permissions :new? do
+      it 'should allow this action' do
+        expect(subject).to permit(editor)
+      end
+    end
+
+    permissions :edit? do
+      it 'should allow this action' do
+        expect(subject).to permit(editor)
+      end
+    end
+
+    permissions :create? do
+      it 'should allow this action' do
+        expect(subject).to permit(editor)
+      end
+    end
+
+    permissions :update? do
+      it 'should allow this action' do
+        expect(subject).to permit(editor)
+      end
+    end
+
+    permissions :destroy? do
+      it 'should allow this action' do
+        expect(subject).to permit(editor)
+      end
+    end
   end
 
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+  describe 'as an viewer user' do
+    subject { described_class }
 
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+    permissions :index? do
+      it 'should allow this action' do
+        expect(subject).to_not permit(viewer)
+      end
+    end
 
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
+    permissions :new? do
+      it 'should allow this action' do
+        expect(subject).to_not permit(viewer)
+      end
+    end
+
+    permissions :edit? do
+      it 'should allow this action' do
+        expect(subject).to_not permit(viewer)
+      end
+    end
+
+    permissions :create? do
+      it 'should allow this action' do
+        expect(subject).to_not permit(viewer)
+      end
+    end
+
+    permissions :update? do
+      it 'should allow this action' do
+        expect(subject).to_not permit(viewer)
+      end
+    end
+
+    permissions :destroy? do
+      it 'should allow this action' do
+        expect(subject).to_not permit(viewer)
+      end
+    end
   end
 end
